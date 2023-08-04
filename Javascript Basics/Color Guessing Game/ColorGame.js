b014a1886da29f05dcd35a97e9421d7e4f99bb53
Stacey Lance <sklance@anderson.edu>
2023-08-03 23:37:42 -0400

    Added Reset Button, abstracted certain items to functions

--- Javascript Basics/Color Guessing Game/ColorGame.js
+++ Javascript Basics/Color Guessing Game/ColorGame.js
@@ -11,6 +11,7 @@ var colors = [
 var WinningRgb = "rgb(0, 0, 0)";
 
 function Randomize (colors, SquaresLength){
+//Creates randomized colors for color array
     var Red = 0, Blue = 0, Green = 0;
     for (let i = 0; i < SquaresLength; i++) {
         Red = Math.floor(Math.random() * 256);
@@ -18,29 +19,52 @@ function Randomize (colors, SquaresLength){
         Green = Math.floor(Math.random() * 256);
         colors[i]=("rgb(" + Red + ", " + Blue) + ", " + Green + ")";
     }
-    WinningRgb = colors[Math.floor(Math.random() * 7)];
+    WinningRgb = colors[Math.floor(Math.random() * (SquaresLength+1))];
 }
 
 
 function Update(squares, colors){
+    //Handles Constant Game Play element
     for (let i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = colors[i];
         squares[i].addEventListener("click", function(){
             var clickedColor = this.style.backgroundColor;
-            if(clickedColor === WinningRgb){
-                for (let j = 0; j < squares.length; j++) {
-                    squares[j].style.backgroundColor = WinningRgb;
-                    h1.style.backgroundColor = WinningRgb;
-                };
-                MessageDisplay.textContent = "Correct!";
-            } else {
-                this.style.backgroundColor = "#232323";
-                MessageDisplay.textContent = "Try Again";
-            };
+            var clickedObject = this;
+            ColorCheck(clickedColor, WinningRgb, clickedObject);
         })};
     document.getElementById("WinningRgb").textContent = WinningRgb;
+    Reset.addEventListener("click", function(){
+        ResetAll(colors, 6);
+    });
+    Easy.addEventListener("click", function() {
+        EasyMode();
+    });
+    Hard.addEventListener("click", function(){
+        HardMode();
+    });
 };
 
+function ColorCheck(clickedColor, WinningRgb, clickedObject){
+//Performs color check and winning / losing actions
+    if(clickedColor === WinningRgb){
+        for (let j = 0; j < squares.length; j++) {
+            squares[j].style.backgroundColor = WinningRgb;
+            h1.style.backgroundColor = WinningRgb;
+            MessageDisplay.textContent = "Correct!";
+        };
+    } else {
+        clickedObject.style.backgroundColor = "#232323";
+        MessageDisplay.textContent = "Try Again";
+        };
+};
+
+function ResetAll(colors, SquaresLength){
+//Resets colors and backgrounds;
+Randomize(colors, SquaresLength);
+Update(squares, colors);
+h1.style.backgroundColor = "#232323";
+MessageDisplay.textContent="";
+}
 
 //Dom Manipulation Call
 var squares = document.querySelectorAll(".square");
