8d89cff66c8c4939725f76c07aab88a24bb45594
Stacey Lance <sklance@anderson.edu>
2023-08-05 16:02:28 -0400

    aesthetic changes, added selection functionality.

--- Javascript Basics/Color Guessing Game/ColorGame.js
+++ Javascript Basics/Color Guessing Game/ColorGame.js
@@ -1,12 +1,12 @@
 console.log("Connected!");
 //variable / function call
 var colors = [
-    "rgb(0, 0, 255)",
-    "rgb(0, 255, 255)",
-    "rgb(255, 0, 0)",
-    "rgb(255, 0, 255)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)"
+    "#232323",
+    "#232323",
+    "#232323",
+    "#232323",
+    "#232323",
+    "#232323"
 ];
 var WinningRgb = "rgb(0, 0, 0)";
 var SquaresLength = 6;
@@ -20,11 +20,14 @@ function Randomize (colors, SquaresLength){
         Green = Math.floor(Math.random() * 256);
         colors[i]=("rgb(" + Red + ", " + Blue) + ", " + Green + ")";
     }
-    WinningRgb = colors[Math.floor(Math.random() * (SquaresLength+1))];
+    for (let l = SquaresLength; l < squares.length; l++) {
+        colors[l]="#232323";
+    }
+    WinningRgb = colors[Math.floor(Math.random() * (SquaresLength))];
 }
 
 
-function Update(squares, colors){
+function Update(squares, colors, SquaresLength){
     //Handles Constant Game Play element
     for (let i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = colors[i];
@@ -34,24 +37,16 @@ function Update(squares, colors){
             ColorCheck(clickedColor, WinningRgb, clickedObject);
         })};
     document.getElementById("WinningRgb").textContent = WinningRgb;
-    Reset.addEventListener("click", function(){
-        ResetAll(colors, SquaresLength);
-    });
-    Easy.addEventListener("click", function() {
-        EasyMode(colors, SquaresLength);
-    });
-    Hard.addEventListener("click", function(){
-        HardMode(colors, SquaresLength);
-    });
 };
 
 function ColorCheck(clickedColor, WinningRgb, clickedObject){
 //Performs color check and winning / losing actions
     if(clickedColor === WinningRgb){
-        for (let j = 0; j < squares.length; j++) {
+        for (let j = 0; j < SquaresLength; j++) {
             squares[j].style.backgroundColor = WinningRgb;
             h1.style.backgroundColor = WinningRgb;
             MessageDisplay.textContent = "Correct!";
+            Reset.textContent = "Play again?"
         };
     } else {
         clickedObject.style.backgroundColor = "#232323";
@@ -62,17 +57,18 @@ function ColorCheck(clickedColor, WinningRgb, clickedObject){
 function ResetAll(colors, SquaresLength){
 //Resets colors and backgrounds;
     Randomize(colors, SquaresLength);
-    Update(squares, colors);
+    Update(squares, colors, SquaresLength);
     h1.style.backgroundColor = "#232323";
     MessageDisplay.textContent="";
+    Reset.textContent = "New Colors";
 }
 
-function HardMode(colors, SquaresLength) {
+function HardMode(colors) {
     SquaresLength = 6;
     ResetAll(colors, SquaresLength);
 }
 
-function EasyMode(colors, SquaresLength){
+function EasyMode(colors){
     SquaresLength = 3;
     ResetAll(colors, SquaresLength);
 }
@@ -84,8 +80,22 @@ var h1 = document.querySelector("h1");
 var Reset = document.querySelector("#Reset");
 var Easy = document.querySelector("#Easy");
 var Hard = document.querySelector("#Hard");
-var playingDiv = document.querySelector(".container");
-var PlaySpaces = playingDiv.querySelectorAll("div");
+Reset.addEventListener("click", function(){
+    ResetAll(colors, SquaresLength);
+});
 
-Randomize(colors, 6);
-Update(squares, colors);
+Easy.addEventListener("click", function() {
+    EasyMode(colors);
+    Easy.classList.add("selected");
+    Hard.classList.remove("selected");
+});
+
+Hard.addEventListener("click", function(){
+    HardMode(colors);
+    Easy.classList.remove("selected");
+    Hard.classList.add("selected");
+});
+
+
+Randomize(colors, SquaresLength);
+Update(squares, colors, SquaresLength);
