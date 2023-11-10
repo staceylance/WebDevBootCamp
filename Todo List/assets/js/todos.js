3e3d55b31718feb5502fdc6ff1281443819d98a6
Stacey Lance <sklance@anderson.edu>
2023-11-10 01:23:11 -0500

    added jquery functionality for deletion and addition of items

---
+++ Todo List/assets/js/todos.js
@@ -0,0 +1,19 @@
+// Check off Specific ToDos by clicking
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+// Click on X to delete Todo
+$("ul").on("click", "span", function(){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+// Add new Todo
+$("#NewTodo").on("change", function(){
+    var NewLi = "<li><span>X</span> " + $(this).val() + "</li>";
+    $(this).val("");
+    $("ul").append(NewLi);
+});
