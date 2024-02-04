3e4371cade3cecd1574e95300b39825c9f03ff10
staceylance <sklance@anderson.edu>
2024-02-04 03:55:20 +0000

    added comment to explain object

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -5,6 +5,8 @@ app.get("/", function(req, res){
    res.render("home.ejs"); 
 });
 
+
+//the brackets in the render allows transferance of objects.
 app.get("/fallinlovewith/:thing", function(req, res){
     var thing = req.params.thing;
     res.render("love.ejs", {thing: thing});
