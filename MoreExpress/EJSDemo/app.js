35aec808cb2d8c3ffb0f759aa09e8210ec09aeeb
staceylance <sklance@anderson.edu>
2024-02-20 18:33:49 +0000

    added template for header and footer.

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -1,15 +1,21 @@
 var express = require("express");
 var app = express();
 
+//had to notify express to look in that folder for assets.
+app.use(express.static("public"));
+
+//Tells express to expect all pages to be ejs files. avoids needing to add .ejs at end of page names
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-   res.render("home.ejs"); 
+   res.render("home"); 
 });
 
 
 //the brackets in the render allows transferance of objects.
 app.get("/fallinlovewith/:thing", function(req, res){
     var thing = req.params.thing;
-    res.render("love.ejs", {thing: thing});
+    res.render("love", {thing: thing});
 })
 
 app.get("/posts", function(req,res){
@@ -19,7 +25,7 @@ app.get("/posts", function(req,res){
         {title: "Can you believe this pomsky?", author: "Colt"}
         ];
 //first item in brackets is name inside of rendered ejs file.
-    res.render("posts.ejs", {posts: posts});
+    res.render("posts", {posts: posts});
 })
 app.listen(process.env.PORT, process.env.IP, function(){
  console.log("Server is listening!")   
