334c96ab63ec02ebe9dcb31cce783c32b6199378
staceylance <sklance@anderson.edu>
2024-02-20 17:37:53 +0000

    Added posts.ejs to illustrate for loop inside ejs page

--- MoreExpress/EJSDemo/app.js
+++ MoreExpress/EJSDemo/app.js
@@ -12,6 +12,15 @@ app.get("/fallinlovewith/:thing", function(req, res){
     res.render("love.ejs", {thing: thing});
 })
 
+app.get("/posts", function(req,res){
+    var posts = [
+        {title: "Post 1", author: "Susy"},
+        {title: "My adorable pet bunny", author: "Charlie"},
+        {title: "Can you believe this pomsky?", author: "Colt"}
+        ];
+//first item in brackets is name inside of rendered ejs file.
+    res.render("posts.ejs", {posts: posts});
+})
 app.listen(process.env.PORT, process.env.IP, function(){
  console.log("Server is listening!")   
 });
