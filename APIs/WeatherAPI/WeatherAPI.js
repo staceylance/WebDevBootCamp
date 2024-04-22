9df2ccb63d1bc80a7e228e63e775547d94dd1fc8
staceylance <59324286+staceylance@users.noreply.github.com>
2024-04-22 18:29:37 -0400

    Update WeatherAPI.js
    Had to index CurrentConditions.

--- APIs/WeatherAPI/WeatherAPI.js
+++ APIs/WeatherAPI/WeatherAPI.js
@@ -3,4 +3,12 @@ import fetch from 'node-fetch';
 var results = await fetch ('http://www.wttr.in/Detroit?format=j1');
 var body = await results.text();
 var parsedData = JSON.parse(body)
-console.log(parsedData);
+var CurrentCondition = parsedData["current_condition"][0];
+
+if (CurrentCondition.FeelsLikeF == CurrentCondition.temp_F) {
+    var temperatureString = "it feels like " + CurrentCondition.FeelsLikeF + " because it is " + CurrentCondition.temp_F
+} else {
+    var temperatureString = "it feels like " + CurrentCondition.FeelsLikeF + ". But it is actually " + CurrentCondition.temp_F
+};
+console.log(temperatureString);
+console.log("Temperatures taken at " + CurrentCondition.observation_time);
