108622e3692ba8aeacd532ad53cd7fcb7c3b1c6b
staceylance <sklance@anderson.edu>
2024-06-08 18:53:01 +0000

    added header / footer logic. Added Bootstrap CDN

--- APIs/YelpCamp/V1/app.js
+++ APIs/YelpCamp/V1/app.js
@@ -10,6 +10,14 @@ app.get("/", function(req,res){
     res.render("home")  
 });
 
+app.get("/campgrounds", function(req,res){
+    res.render("campgrounds")
+});
+
+app.post("/campgrounds", function(req,res){
+    //TODO add posting for creation of new campground.
+});
+
 //Setup port for listening
 app.listen(process.env.PORT, process.env.IP, function(){
  console.log("YelpCamp is online!")   
